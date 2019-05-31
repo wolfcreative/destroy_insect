b9ae7cfd95414b7932096af39eb070557d231751
hiddenski <loonysnow@gmail.com>
2019-05-31 15:00:40 +0500

    update

--- js/all.js
+++ js/all.js
@@ -1,3 +1,9 @@
 $(function() {
 
+    UIkit.util.on('.case .slider', 'itemshow', function (e) {
+        let index = UIkit.util.index(e.target);
+        
+        $('.case .info>div').removeClass('uk-active');
+        $('.case .info').children().eq(index).addClass('uk-active');
+    });
 });
